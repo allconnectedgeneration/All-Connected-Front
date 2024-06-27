ec70f7818bb776a0170b27cef12e1ae3ab1cff6d
Thamires Cavalcante <thamires.araujo1996@gmail.com>
2024-06-27 15:10:57 -0300

    Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
+import Login from './pages/login/Login';
+
+
+
+
 
 function App() {
   return (
-    <>
-    <h1 className="text-3xl font-bold text-red-500 underline text-center">
-      Hello world!
-    </h1>
-    </>
+    <Login />
 );
 }
 export default App;
